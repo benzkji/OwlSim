58b8536d387f08a59d806466b4580defef04f666
benzkji <bnzk@bnzk.ch>
2015-03-02 01:27:17 +0100

    minor things, for I can develop OwlPatches

--- JuceLibraryCode/modules/juce_audio_basics/juce_audio_basics.cpp
+++ JuceLibraryCode/modules/juce_audio_basics/juce_audio_basics.cpp
@@ -53,10 +53,22 @@
 #endif
 
 #if (JUCE_MAC || JUCE_IOS) && JUCE_USE_VDSP_FRAMEWORK
+    #define Point CarbonDummyPointName // (workaround to avoid definition of "Point" by old Carbon headers)
+    #include <Accelerate/Accelerate.h>
+    #undef Point
+#else
+    #undef JUCE_USE_VDSP_FRAMEWORK
+#endif
+
+/*
+benzkji changed this
+http://www.juce.com/forum/topic/build-problem-using-xcode-6
+#if (JUCE_MAC || JUCE_IOS) && JUCE_USE_VDSP_FRAMEWORK
  #include <Accelerate/Accelerate.h>
 #else
  #undef JUCE_USE_VDSP_FRAMEWORK
 #endif
+*/
 
 #if __ARM_NEON__ && ! (JUCE_USE_VDSP_FRAMEWORK || defined (JUCE_USE_ARM_NEON))
  #define JUCE_USE_ARM_NEON 1
